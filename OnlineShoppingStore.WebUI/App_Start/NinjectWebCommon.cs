397af7b0dbdd2ec506eef7ff74ac63e8329919a6
wveik <defender>
2015-03-17 13:00:34 +0300

    Second

--- OnlineShoppingStore.WebUI/App_Start/NinjectWebCommon.cs
+++ OnlineShoppingStore.WebUI/App_Start/NinjectWebCommon.cs
@@ -1,8 +1,7 @@
 [assembly: WebActivatorEx.PreApplicationStartMethod(typeof(OnlineShoppingStore.WebUI.App_Start.NinjectWebCommon), "Start")]
 [assembly: WebActivatorEx.ApplicationShutdownMethodAttribute(typeof(OnlineShoppingStore.WebUI.App_Start.NinjectWebCommon), "Stop")]
 
-namespace OnlineShoppingStore.WebUI.App_Start
-{
+namespace OnlineShoppingStore.WebUI.App_Start {
     using System;
     using System.Web;
 
@@ -10,46 +9,43 @@ namespace OnlineShoppingStore.WebUI.App_Start
 
     using Ninject;
     using Ninject.Web.Common;
+    using Moq;
+    using OnlineShoppingStore.Domain.Abstract;
+    using OnlineShoppingStore.Domain.Entities;
+    using System.Collections.Generic;
 
-    public static class NinjectWebCommon 
-    {
+    public static class NinjectWebCommon {
         private static readonly Bootstrapper bootstrapper = new Bootstrapper();
 
         /// <summary>
         /// Starts the application
         /// </summary>
-        public static void Start() 
-        {
+        public static void Start() {
             DynamicModuleUtility.RegisterModule(typeof(OnePerRequestHttpModule));
             DynamicModuleUtility.RegisterModule(typeof(NinjectHttpModule));
             bootstrapper.Initialize(CreateKernel);
         }
-        
+
         /// <summary>
         /// Stops the application.
         /// </summary>
-        public static void Stop()
-        {
+        public static void Stop() {
             bootstrapper.ShutDown();
         }
-        
+
         /// <summary>
         /// Creates the kernel that will manage your application.
         /// </summary>
         /// <returns>The created kernel.</returns>
-        private static IKernel CreateKernel()
-        {
+        private static IKernel CreateKernel() {
             var kernel = new StandardKernel();
-            try
-            {
+            try {
                 kernel.Bind<Func<IKernel>>().ToMethod(ctx => () => new Bootstrapper().Kernel);
                 kernel.Bind<IHttpModule>().To<HttpApplicationInitializationHttpModule>();
 
                 RegisterServices(kernel);
                 return kernel;
-            }
-            catch
-            {
+            } catch {
                 kernel.Dispose();
                 throw;
             }
@@ -59,8 +55,15 @@ namespace OnlineShoppingStore.WebUI.App_Start
         /// Load your modules or register your services here!
         /// </summary>
         /// <param name="kernel">The kernel.</param>
-        private static void RegisterServices(IKernel kernel)
-        {
-        }        
+        private static void RegisterServices(IKernel kernel) {
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(new List<Product> {
+                new Product { Name = "Football", Price = 23},
+                new Product { Name = "Surf board", Price = 179},
+                new Product { Name = "Running shoes", Price = 95},
+            });
+
+            kernel.Bind<IProductRepository>().ToConstant(mock.Object);
+        }
     }
 }
---
+++ OnlineShoppingStore.WebUI/Controllers/ProductController.cs
@@ -0,0 +1,21 @@
+﻿using OnlineShoppingStore.Domain.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OnlineShoppingStore.WebUI.Controllers {
+    public class ProductController : Controller {
+
+        private readonly IProductRepository repository;
+
+        public ProductController(IProductRepository repo) {
+            repository = repo;
+        }
+
+        public ViewResult List() {
+            return View(repository.Products);
+        }
+    }
+}
