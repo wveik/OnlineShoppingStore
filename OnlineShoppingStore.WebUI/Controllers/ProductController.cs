398ff47de08a0661186935b169b9d6c5fffa7b5b
wveik <defender>
2015-03-24 14:56:24 +0300

    Постраничная Навигация

--- OnlineShoppingStore.WebUI/Controllers/ProductController.cs
+++ OnlineShoppingStore.WebUI/Controllers/ProductController.cs
@@ -1,4 +1,5 @@
 ﻿using OnlineShoppingStore.Domain.Abstract;
+using OnlineShoppingStore.WebUI.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,13 +10,28 @@ namespace OnlineShoppingStore.WebUI.Controllers {
     public class ProductController : Controller {
 
         private readonly IProductRepository repository;
+        public int PageSize = 4;
 
         public ProductController(IProductRepository repo) {
             repository = repo;
         }
 
-        public ViewResult List() {
-            return View(repository.Products);
+        public ViewResult List(int page = 1) {
+
+            ProductsListViewModel model = new ProductsListViewModel {
+                Products = repository.Products
+                .OrderBy(p => p.ProductId)
+                .Skip((page - 1) * PageSize)
+                .Take(PageSize),
+
+                PagingInfo = new PagingInfo { 
+                    CurrentPage = page,
+                    ItemsPerPage = PageSize,
+                    TotalItems = repository.Products.Count()
+                }
+            };
+
+            return View(model);
         }
     }
 }
---
+++ OnlineShoppingStore.WebUI/Models/ProductsListViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using OnlineShoppingStore.Domain.Entities;
+
+namespace OnlineShoppingStore.WebUI.Models {
+    public class ProductsListViewModel {
+        public IEnumerable<Product> Products { get; set; }
+        public PagingInfo PagingInfo { get; set; }
+    }
+}
