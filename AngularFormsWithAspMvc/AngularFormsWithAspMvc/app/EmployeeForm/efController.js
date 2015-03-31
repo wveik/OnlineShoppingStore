49dc30b12cfd2994bc75d20fecd061d35436a973
wveik <defender>
2015-03-31 16:46:26 +0300

    Первый commit AngularJS

---
+++ AngularFormsWithAspMvc/AngularFormsWithAspMvc/app/EmployeeForm/efController.js
@@ -0,0 +1,12 @@
+﻿angularFormsApp.controller('efController',
+    function EmployeeFormController($scope, efService) {
+        $scope.employee = efService.employee;
+
+        $scope.departments = [
+                "Инжинер",
+                "Маркетинг",
+                "Финансовый",
+                "Администратор",
+        ];
+    }
+);
